8c35436def74fa27c46d9ec4b9a23973d35b49bc
jun.calupcupan <jun.calupcupan@DESKTOP-KUGSJMJ>
2024-03-26 10:15:08 +1300

    Add Transaction Model

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿    namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+
+        // CategoryId
+
+        public int Amount { get; set; }
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
