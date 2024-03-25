42cd710995c1f1fcb86c9fd46398c1f63b1a2036
jun.calupcupan <jun.calupcupan@DESKTOP-KUGSJMJ>
2024-03-26 10:19:54 +1300

    Add ApplicationDbContext

---
+++ Expense Tracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
